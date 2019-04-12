78d37d4b651fbc0611d5a3763be94213112ea40f
antoniosehk <antonioszeto@gmail.com>
2019-04-12 12:05:04 -0400

    20190412v5

--- hello_CheXNet_features.py
+++ hello_CheXNet_features.py
@@ -19,14 +19,13 @@ def main():
     # preprocess images
     x_test = chexnet_preprocess_input(image_numpy_array)
 
-    # predict the probability across all output classes
-    yhat = model.predict(x_test)
+    # obtain the feature
+    yhat = base_model.predict(x_test)
 
-    # for each image, print the class names and the probability
+    # for each image, print the feature
     for image_index in range(len(yhat)):
         print(image_path_list[image_index])
-        for i in range(len(yhat[image_index])):
-            print(chexnet_class_index_to_name[i], yhat[image_index][i])
+        print(len(yhat[image_index]), yhat[image_index])
 
 
 if __name__ == '__main__':
@@ -34,21 +33,6 @@ if __name__ == '__main__':
     What this script is doing is that
     1. It first loads a chexnet nodel (DenseNet121),
        where the weight is from https://github.com/brucechou1983/CheXNet-Keras
-    2. It then makes predictions on the images on the image_path_list
-    3. The probability of prediction across all 14 classes is computed
-        'Atelectasis',  # 0
-        'Cardiomegaly',  # 1
-        'Effusion',  # 2
-        'Infiltration',  # 3
-        'Mass',  # 4
-        'Nodule',  # 5
-        'Pneumonia',  # 6
-        'Pneumothorax',  # 7
-        'Consolidation',  # 8
-        'Edema',  # 9
-        'Emphysema',  # 10
-        'Fibrosis',  # 11
-        'Pleural_Thickening',  # 12
-        'Hernia',  # 13
+    2. It then outputs the activation output of the layer before the output layer
     '''
     main()
--- hello_tCheXNet.py
+++ hello_tCheXNet.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     '''
     What this script is doing is that
     1. It first loads a tCheXNet nodel
-    2. It then made predictions on the images on the image_path_list
+    2. It then makes predictions on the images on the image_path_list
     3. The probability of prediction across Pneumothorax is computed
     '''
     main()
